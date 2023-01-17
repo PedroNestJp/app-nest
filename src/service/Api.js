9e3fcc8f7c50478a045d9c9cd8a5cb28d3bf0681
Pedro Nest <pedronestjp@gmail.com>
2023-01-16 22:41:39 -0300

    up api, url,mysql

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"https://nest-app.herokuapp.com"
+    baseURL:"mysql://kqt9aeyvsabaauen:irbtmhgqg823b1ib@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ezer9ndy7x4h2g05"
 })
 
 export default api
